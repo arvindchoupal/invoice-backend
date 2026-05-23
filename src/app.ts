7713a706719d625d4c3579521bdb3681d2053142
arvindchoupal <arvindchoupal1999@gmail.com>
2026-05-23 12:12:32 +0530

    d

--- src/app.ts
+++ src/app.ts
@@ -23,7 +23,7 @@ import { errorHandler, notFound } from "./middleware/error";
 export const app = express();
 
 app.use(helmet());
-app.use(cors({ origin: env.frontendUrl, credentials: true }));
+app.use(cors());
 app.use(rateLimit({ windowMs: 15 * 60 * 1000, limit: 300 }));
 app.use(express.json({ limit: "2mb" }));
 app.use(express.urlencoded({ extended: true }));
