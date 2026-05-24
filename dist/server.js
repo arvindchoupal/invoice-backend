81471f5000395c3809e86b306a7904d30e7f5924
arvindchoupal <arvindchoupal1999@gmail.com>
2026-05-24 22:43:57 +0530

    asas

--- dist/server.js
+++ dist/server.js
@@ -1,22 +1,20 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = require("./app");
-const db_1 = require("./config/db");
 const env_1 = require("./config/env");
-const server = app_1.app.listen(env_1.env.port, "0.0.0.0", () => {
+app_1.app.listen(env_1.env.port, "0.0.0.0", () => {
     console.log(`Server running on http://localhost:${env_1.env.port}`);
 });
-async function shutdown(signal) {
-    console.log(`${signal}: closing HTTP server and MySQL pool…`);
-    server.close(async () => {
-        try {
-            await db_1.pool.end();
-        }
-        finally {
-            process.exit(0);
-        }
-    });
-    setTimeout(() => process.exit(1), 10_000).unref();
-}
-process.on("SIGTERM", () => void shutdown("SIGTERM"));
-process.on("SIGINT", () => void shutdown("SIGINT"));
+// async function shutdown(signal: string) {
+//   console.log(`${signal}: closing HTTP server and MySQL pool…`);
+//   server.close(async () => {
+//     try {
+//       await pool.end();
+//     } finally {
+//       process.exit(0);
+//     }
+//   });
+//   setTimeout(() => process.exit(1), 10_000).unref();
+// }
+// process.on("SIGTERM", () => void shutdown("SIGTERM"));
+// process.on("SIGINT", () => void shutdown("SIGINT"));
--- src/server.ts
+++ src/server.ts
@@ -2,21 +2,21 @@ import { app } from "./app";
 import { pool } from "./config/db";
 import { env } from "./config/env";
 
-const server = app.listen(env.port, "0.0.0.0", () => {
+app.listen(env.port, "0.0.0.0", () => {
   console.log(`Server running on http://localhost:${env.port}`);
 });
 
-async function shutdown(signal: string) {
-  console.log(`${signal}: closing HTTP server and MySQL pool…`);
-  server.close(async () => {
-    try {
-      await pool.end();
-    } finally {
-      process.exit(0);
-    }
-  });
-  setTimeout(() => process.exit(1), 10_000).unref();
-}
+// async function shutdown(signal: string) {
+//   console.log(`${signal}: closing HTTP server and MySQL pool…`);
+//   server.close(async () => {
+//     try {
+//       await pool.end();
+//     } finally {
+//       process.exit(0);
+//     }
+//   });
+//   setTimeout(() => process.exit(1), 10_000).unref();
+// }
 
-process.on("SIGTERM", () => void shutdown("SIGTERM"));
-process.on("SIGINT", () => void shutdown("SIGINT"));
+// process.on("SIGTERM", () => void shutdown("SIGTERM"));
+// process.on("SIGINT", () => void shutdown("SIGINT"));
