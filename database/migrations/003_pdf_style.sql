19788a2c07c51c02d3e42465329008a54c8359ee
arvindchoupal <arvindchoupal1999@gmail.com>
2026-05-27 20:54:49 +0530

    ds

--- database/migrations/003_pdf_style.sql
+++ database/migrations/003_pdf_style.sql
@@ -1,5 +1,9 @@
 ALTER TABLE invoices
-  ADD COLUMN IF NOT EXISTS pdf_style VARCHAR(32) NOT NULL DEFAULT 'classic' AFTER terms;
+ADD COLUMN pdf_style VARCHAR(32)
+NOT NULL DEFAULT 'classic'
+AFTER terms;
 
 ALTER TABLE settings
-  ADD COLUMN IF NOT EXISTS default_pdf_style VARCHAR(32) NOT NULL DEFAULT 'classic' AFTER invoice_counter;
+ADD COLUMN default_pdf_style VARCHAR(32)
+NOT NULL DEFAULT 'classic'
+AFTER invoice_counter;
